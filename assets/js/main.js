e0d35563d64f2a6cf65644edf528dfce23a1397c
Mrh Ripon <stackbind@gmail.com>
2023-10-30 00:04:47 +0600

    index page under adjust

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,4 @@
 document.addEventListener('DOMContentLoaded', function(){
-
-  
-    // Testimonial Activation for Splide js plugin 
-    let testimonial = new Splide("#testimonialCarousel", {
-        speed: 1000,
-        type: "loop",
-        perPage: 4,
-        gap: 30,
-        rewind: true,
-        centerMode: true,
-        pagination: false,
-        breakpoints: {
-          1024: {
-            perPage: 2,
-          },
-          480: {
-            perPage: 1,
-            gap: 16,
-            autoHeight: true,
-          },
-        },
-      });
-    
-      testimonial.mount();
-
-
-
       // Mobile Menu Activation
       if(document.getElementById('toggle-bar') !== null) {
         document.getElementById('toggle-bar').addEventListener('click', function(){
